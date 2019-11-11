6503e7648618f6f2cda5c9a10cb83487d665e3e0
Sheril Elizabeth Jose <sheril@Sherils-MacBook-Pro.local>
2019-11-11 21:53:54 +0100

    Adding Student class

---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Gradebook.Enums;
+
+namespace Gradebook
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public StudentType Type { get; set; }
+
+        public EnrollmentType Enrollment { get; set; }
+
+        public List<double> Grades { get; set; }
+        [JsonIgnore]
+
+        public double AverageGrade
+        {
+            get
+            {
+                return Grades.Average();
+            }
+        }
+        [JsonIgnore]
+
+        public char LetterGrade { get; set; }
+        [JsonIgnore]
+    }
+}
